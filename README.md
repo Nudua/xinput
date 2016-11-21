68132433e1db76463e8d8e8052d7f00ed2e42773
Tor Ramstad <Tor Ramstad>
2016-11-21 03:02:39 +0100

    Analog to digital input, updated readme.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 xinput
 ======
 
-XInput (XBOX 360 compatible controller) library for [Go][] on Windows.
+XInput (Xbox 360/Xbox One compatible controller) library for [Go][] on Windows.
+
+Based on: https://github.com/tajtiattila/xinput
+
+Modified version that combines analog input into digital and gives access to modify deadzones.
 
 [Go]: http://golang.org
 
@@ -12,21 +16,32 @@ Requirements
 - Windows 7: xinput1_3.dll
 - Windows Vista: xinput9_1_0.dll
 
-Tested on Windows 7 x64.
+Tested on Windows 10 x64.
 
 Installation
 ------------
 
-`go get github.com/tajtiattila/xinput`
+`go get github.com/Nudua/xinput`
 
 Usage
 -----
 
-Only three functions are available.
+`IsLoaded` checks if XInput was loaded successfully.
 
-`Load` loads xinput.dll. Not strictly necessary, but useful for
-checking whether xinput is actually available.
+`GetState` retrieves the state thumbsticks, d-pad, buttons, triggers and digital versions of the analog input.
 
-`GetState` retrieves the state thumbsticks, d-pad, buttons and triggers.
+`GetSimpleState` retrieves the state thumbsticks, d-pad, buttons and triggers.
 
 `SetState` sets vibration state.
+
+Deadzones and thresholds for analog inputs
+---------
+`LEFT_THUMB_DEADZONE` default '7849' (-32767 to 32767)
+
+`RIGHT_THUMB_DEADZONE` default '8689' (-32767 to 32767)
+
+`TRIGGER_TRESHOLD` default '50' (0 to 255)
+
+Example
+-------
+TODO
--- xinput.go
+++ xinput.go
@@ -10,8 +10,6 @@ type State struct {
 	Gamepad      Gamepad
 }
 
-type Button uint16
-
 type Gamepad struct {
 	Buttons      uint16
 	LeftTrigger  uint8
@@ -27,28 +25,52 @@ type Vibration struct {
 	RightMotorSpeed uint16
 }
 
-const (
-	CONTROLLER_MAX       = 4 // valid controller numbers are 0-3
-	TRIGGER_TRESHOLD     = 30
-	LEFT_THUMB_DEADZONE  = 7849
-	RIGHT_THUMB_DEADZONE = 8689
+// CONTROLLER_MAX valid controller numbers are 0-3
+const CONTROLLER_MAX = 4
+
+var (
+	// TRIGGER_TRESHOLD Threshold for the left and right triggers (0 to 255)
+	TRIGGER_TRESHOLD uint8 = 50
+
+	// LEFT_THUMB_DEADZONE Deadzone for the Left Thumb Stick (-32767 to 32767)
+	LEFT_THUMB_DEADZONE int16 = 7849
+
+	// LEFT_THUMB_DEADZONE Deadzone for the Right Thumb Stick (-32767 to 32767)
+	RIGHT_THUMB_DEADZONE int16 = 8689
 )
 
+// Digital Input
 const (
-	DPAD_UP        Button = 0x0001
-	DPAD_DOWN      Button = 0x0002
-	DPAD_LEFT      Button = 0x0004
-	DPAD_RIGHT     Button = 0x0008
-	START          Button = 0x0010
-	BACK           Button = 0x0020
-	LEFT_THUMB     Button = 0x0040
-	RIGHT_THUMB    Button = 0x0080
-	LEFT_SHOULDER  Button = 0x0100
-	RIGHT_SHOULDER Button = 0x0200
-	BUTTON_A       Button = 0x1000
-	BUTTON_B       Button = 0x2000
-	BUTTON_X       Button = 0x4000
-	BUTTON_Y       Button = 0x8000
+	DPAD_UP        uint = 0x0001
+	DPAD_DOWN      uint = 0x0002
+	DPAD_LEFT      uint = 0x0004
+	DPAD_RIGHT     uint = 0x0008
+	START          uint = 0x0010
+	BACK           uint = 0x0020
+	LEFT_THUMB     uint = 0x0040
+	RIGHT_THUMB    uint = 0x0080
+	LEFT_SHOULDER  uint = 0x0100
+	RIGHT_SHOULDER uint = 0x0200
+	BUTTON_A       uint = 0x1000
+	BUTTON_B       uint = 0x2000
+	BUTTON_X       uint = 0x4000
+	BUTTON_Y       uint = 0x8000
+)
+
+// Analog to Digital Input
+const (
+	RIGHT_STICK_UP    uint = 0x10000
+	RIGHT_STICK_DOWN  uint = 0x20000
+	RIGHT_STICK_LEFT  uint = 0x40000
+	RIGHT_STICK_RIGHT uint = 0x80000
+
+	LEFT_STICK_UP    uint = 0x100000
+	LEFT_STICK_DOWN  uint = 0x200000
+	LEFT_STICK_LEFT  uint = 0x400000
+	LEFT_STICK_RIGHT uint = 0x800000
+
+	LEFT_TRIGGER  uint = 0x1000000
+	RIGHT_TRIGGER uint = 0x2000000
 )
 
 var (
@@ -89,13 +111,86 @@ func IsLoaded() (bool, error) {
 	return loadError == nil, loadError
 }
 
-// GetState retrieves the current state of the controller.
-func GetState(controller uint, state *State) error {
+// GetState retrieves the current state of the controller including analog buttons as digital.
+// The analog inputs (Thumbsticks and Triggers) will be shifted into digitalAnalogButtonState
+func GetState(controller uint, state *State, digitalAnalogButtonState *uint) error {
 	r, _, _ := procXInputGetState.Call(uintptr(controller), uintptr(unsafe.Pointer(state)))
+
 	if r == 0 {
 		return nil
 	}
+
+	*digitalAnalogButtonState = uint(state.Gamepad.Buttons)
+
+	analogToDigital(digitalAnalogButtonState, state)
+
 	return syscall.Errno(r)
+}
+
+// GetSimpleState retrieves the current state of the controller excluding analog buttons as digital.
+func GetSimpleState(controller uint, state *State) error {
+	r, _, _ := procXInputGetState.Call(uintptr(controller), uintptr(unsafe.Pointer(state)))
+
+	if r == 0 {
+		return nil
+	}
+
+	return syscall.Errno(r)
+}
+
+// IsDown Example: IsDown(allButtonState, DPAD_UP)
+func IsDown(digitalAnalogButtonState uint, button uint) bool {
+	return (digitalAnalogButtonState & button) == button
+}
+
+func analogToDigital(digitalAnalogButtonState *uint, state *State) {
+
+	//Left Thumbstick
+	if state.Gamepad.ThumbLX > LEFT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= LEFT_STICK_RIGHT
+
+	} else if state.Gamepad.ThumbLX < -LEFT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= LEFT_STICK_LEFT
+	}
+
+	if state.Gamepad.ThumbLY > LEFT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= LEFT_STICK_UP
+
+	} else if state.Gamepad.ThumbLY < -LEFT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= LEFT_STICK_DOWN
+	}
+
+	//Right ThumbStick
+	if state.Gamepad.ThumbRX > RIGHT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= RIGHT_STICK_RIGHT
+
+	} else if state.Gamepad.ThumbRX < -RIGHT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= RIGHT_STICK_LEFT
+	}
+
+	if state.Gamepad.ThumbRY > RIGHT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= RIGHT_STICK_UP
+
+	} else if state.Gamepad.ThumbRY < -RIGHT_THUMB_DEADZONE {
+
+		*digitalAnalogButtonState |= RIGHT_STICK_DOWN
+	}
+
+	//Triggers
+	if state.Gamepad.LeftTrigger > TRIGGER_TRESHOLD {
+		*digitalAnalogButtonState |= LEFT_TRIGGER
+	}
+
+	if state.Gamepad.RightTrigger > TRIGGER_TRESHOLD {
+		*digitalAnalogButtonState |= RIGHT_TRIGGER
+	}
 }
 
 // SetState sets the vibration for the controller.
